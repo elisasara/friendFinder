f3b50ee82e41fb362019aecd402eab89ea683a95
Elisa Urda <urda.elisa@gmail.com>
2018-05-08 16:51:27 -0400

    corrected server location

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-require("./routing/apiRoutes.js")(app);
-require("./routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 // server is listening
 app.listen(PORT, function() {
